bd8174b75a633a747e930f5e7573ed2b2ceb5e5f
Achyutashivam <shivam@achyutagroup.com>
2026-05-06 16:21:05 +0530

    Fix 404 and add Property Details page

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://tolet-backend-lpe4.onrender.com/api',
         changeOrigin: true,
         secure: false,
       },
